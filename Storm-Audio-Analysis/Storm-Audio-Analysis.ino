993a44891a6b40d91b6a376de0e4848811ad6f14
Robert Kennedy <helmet0012@gmail.com>
2016-04-13 23:23:01 -0400

    Finished testing the MSGEQ7 audio analysis chip with arduino.

---
+++ Storm-Audio-Analysis/Storm-Audio-Analysis.ino
@@ -0,0 +1,85 @@
+//Declare audio analysis pin connections
+#define STROBE 4
+#define RESET 5
+#define DC_One A0
+#define DC_Two A1 
+
+//Define audio analysis variables
+int band;
+int Frequencies_One[7];
+int Frequencies_Two[7];
+
+//Define led pins
+int ledPins[7] = {13, 12, 11, 10, 9, 8, 7};
+
+/********************Setup Loop*************************/
+void setup() {
+  Serial.begin(9600);
+  
+  //Set spectrum Shield pin configurations
+  pinMode(STROBE, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(DC_One, INPUT);
+  pinMode(DC_Two, INPUT);
+  for (int i=0; i<7; i++) {
+    pinMode(ledPins[i], OUTPUT);  
+  }
+  digitalWrite(STROBE, HIGH);
+  digitalWrite(RESET, HIGH);
+  
+  //Initialize Spectrum Analyzers
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, HIGH);
+  delay(1);
+  digitalWrite(STROBE, HIGH);
+  delay(1);
+  digitalWrite(STROBE, LOW);
+  delay(1);
+  digitalWrite(RESET, LOW);
+}
+
+
+/**************************Main Function Loop*****************************/
+void loop() {
+  Read_Frequencies();
+  leds();
+  //Graph_Frequencies();
+  delay(100);
+}
+
+void leds() {
+  for (int i=0; i<7; i++) {
+    if(Frequencies_One[i] > 300) {
+      digitalWrite(ledPins[i], HIGH);
+    } else {
+      digitalWrite(ledPins[i], LOW);
+    }
+  }
+}
+
+/*******************Pull frquencies from Spectrum Shield********************/
+void Read_Frequencies(){
+  //Read frequencies for each band
+  for (band = 0; band<7; band++)
+  {
+    Frequencies_One[band] = analogRead(DC_One);
+    Frequencies_Two[band] = analogRead(DC_Two); 
+    digitalWrite(STROBE, HIGH);
+    digitalWrite(STROBE, LOW);
+  }
+}
+
+/*******************Light LEDs based on frequencies*****************************/
+void Graph_Frequencies(){
+  Serial.println(Frequencies_One[0]);
+  /*
+   for(int i= 0; i<7; i++)
+   {
+     Serial.println(Frequencies_One[i]);
+     Serial.print(" ");
+     Serial.println(Frequencies_Two[i]);
+     delay(10);
+   }
+   */
+}
