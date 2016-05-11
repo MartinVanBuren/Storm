a1d69b17ce0baf95821ed32207174573e253dd29
Robert Kennedy <helmet0012@gmail.com>
2016-05-11 17:13:00 -0400

    * Moved old jQuery flask project to an "old" folder. * Created new flask project that is angular based.

---
+++ web/stormapp/stormapp.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# return index page when IP address of RPi is typed in the browser
+@app.route("/")
+def Index():
+	return render_template("index.html")
+
+	
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8080, debug=False)
